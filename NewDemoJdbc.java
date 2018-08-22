c624f7a440c3461d7253a1bbed60649c2a79a974
O <catssshead@gmail.com>
2018-08-22 15:18:53 +0200

    Update NewDemoJdbc.java
    Added some examples

--- NewDemoJdbc.java
+++ NewDemoJdbc.java
@@ -39,6 +39,65 @@ public class NewDemoJdbc {
 				
 				System.out.println("Numero client: " + ncVar + ", Nom Client " + nomcVar + ", Ville " + villeVar);
 			}
+			
+			//afficher une article de couleur rouge et poids >2000
+			String sqlP = "SELECT COUL, PDS FROM PRODUITS WHERE COUL = 'rouge' AND PDS>2000";
+			ResultSet resultP = st.executeQuery(sqlP);
+
+			String coulVar;
+			int pdsVar;
+
+			while (resultP.next()) {
+				coulVar = resultP.getString("coul");
+				pdsVar = resultP.getInt("pds");
+
+				System.out.println("Couleur de article: " + coulVar + ", poids d'article: " + pdsVar);
+			}
+
+			String sqlR = "SELECT REPRESENTANTS.NR FROM formation.REPRESENTANTS\r\n" + 
+					"INNER JOIN formation.VENTES\r\n" + 
+					"ON REPRESENTANTS.NR = VENTES.QI\r\n" + 
+					"WHERE QI > 1";
+			ResultSet resultR = st.executeQuery(sqlR);
+
+			int nrVar;
+
+			while (resultR.next()) {
+				nrVar = resultR.getInt("nr");
+
+				System.out.println("Numero de representant est " + nrVar);
+			}
+
+			String sqlV = "SELECT DISTINCT CLIENTS.NOMC FROM formation.CLIENTS\r\n" +
+					"INNER JOIN formation.VENTES\r\n" +
+					"ON CLIENTS.NC = VENTES.NC\r\n"+
+					"WHERE VILLE = 'Lyon'\r\n"+
+					"AND QI > 180";
+
+			ResultSet resultV = st.executeQuery(sqlV);
+
+			String nomcV;
+
+			while (resultV.next()) {
+				nomcV = resultV.getString("nomc");
+
+				System.out.println("Le nom de client est " + nomcV);
+			}
+			
+			String sqlCR = "SELECT DISTINCT REPRESENTANTS.NOMR, CLIENTS.NOMC FROM (((formation.VENTES\n" + 
+					"INNER JOIN formation.PRODUITS ON VENTES.NP = PRODUITS.NP)\n" + 
+					"INNER JOIN formation.REPRESENTANTS ON VENTES.NR = REPRESENTANTS.NR)\n" + 
+					"INNER JOIN formation.CLIENTS ON VENTES.NC = CLIENTS.NC)\n" + 
+					"WHERE PRODUITS.COUL = 'rouge'\n" + 
+					"AND VENTES.QI > 100";
+			
+			ResultSet resultCR = st.executeQuery(sqlCR);
+			
+			String varCR;
+			while (resultCR.next()) {
+				varCR = resultCR.getString("nomc");
+				System.out.println("Les noms de clients sont " + varCR);
+			}
 		} 
 		//dans le catch on met les exeptions
 		catch (ClassNotFoundException e) {
